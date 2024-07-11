4001b3e2c80ee4453c61a8b30ea0bb138d8e3e00
maardgs <mariliarodrigues.94@gmail.com>
2024-07-10 21:39:53 -0300

    First commit

---
+++ src/main/java/com/generation/gamestore/GamesStorelApplication.java
@@ -0,0 +1,13 @@
+package com.generation.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStorelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesStorelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,66 @@
+package com.generation.gamestore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	private String nome;
+	
+	@NotNull(message = "O produto precisa ter um preço")
+	private Float valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+		
+}
